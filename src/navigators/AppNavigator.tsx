69f05cf2983efbdfd3a5c0e49f985be0ab7d5d74
AustinWildgrube <austinwildgrube@gmail.com>
2023-01-26 13:36:14 -0600

    feat: add a bottom tab navigator

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
-import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { ProfileScreen } from 'src/screens/app/ProfileScreen';
 import { useCurrentUser } from '../contexts/AuthProvider';
+import { User } from '@tamagui/lucide-icons';
 
 export type AppStackParamList = {
   Profile: undefined;
@@ -10,21 +12,23 @@ export type AppStackParamList = {
 
 export type AppNavProps<T extends keyof AppStackParamList> = NativeStackNavigationProp<AppStackParamList, T>;
 
-const Stack = createNativeStackNavigator<AppStackParamList>();
+const Tab = createBottomTabNavigator();
 
 export const AppNavigator: FC = (): JSX.Element => {
   const user = useCurrentUser();
 
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name={'Profile'}
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Profile"
         component={ProfileScreen}
         options={{
           headerShown: true,
           title: `@${user.username}`,
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({}) => <User />,
         }}
       />
-    </Stack.Navigator>
+    </Tab.Navigator>
   );
 };
