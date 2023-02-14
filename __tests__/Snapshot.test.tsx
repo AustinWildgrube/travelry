9a52f750a770327cae79b2559cfd44e92045915f
AustinWildgrube <austinwildgrube@gmail.com>
2023-02-13 22:02:16 -0600

    test: create snapshot tests for the new components

--- __tests__/Snapshot.test.tsx
+++ __tests__/Snapshot.test.tsx
@@ -1,26 +1,56 @@
 import { RouteProp } from '@react-navigation/native';
 
+import { ProfileStackParamList } from '&/navigators/profile-navigator';
 import { AppStackParamList } from '&/navigators/root-navigator';
+import { AlbumScreen } from '&/screens/app/AlbumScreen';
 import { CameraScreen } from '&/screens/app/CameraScreen';
+import { EditScreen } from '&/screens/app/EditScreen';
+import { FeedScreen } from '&/screens/app/FeedScreen';
 import { PostScreen } from '&/screens/app/PostScreen';
 import { ProfileScreen } from '&/screens/app/ProfileScreen';
 import { AuthenticationScreen } from '&/screens/authentication/AuthenticationScreen';
 
 import { currentUser, currentUserPosts, wrapRender } from '../jestSetupFile';
 
-describe('snapshot test', () => {
+describe('snapshot tests', () => {
+  it('should properly display the album screen', () => {
+    const albumRouter: RouteProp<ProfileStackParamList, 'Album'> = {
+      key: '',
+      name: 'Album',
+      params: {
+        albumId: '',
+      },
+    };
+
+    const tree = wrapRender(<AlbumScreen route={albumRouter} />);
+    expect(tree).toMatchSnapshot();
+  });
+
   it('should properly display the authentication screen', () => {
     const tree = wrapRender(<AuthenticationScreen />);
     expect(tree).toMatchSnapshot();
   });
 
-  it('should properly display the profile screen', () => {
-    const tree = wrapRender(<ProfileScreen />);
+  it('should properly display the camera screen', () => {
+    const tree = wrapRender(<CameraScreen />);
     expect(tree).toMatchSnapshot();
   });
 
   it('should properly display the camera screen', () => {
-    const tree = wrapRender(<CameraScreen />);
+    const editRouter: RouteProp<AppStackParamList, 'Edit'> = {
+      key: '',
+      name: 'Edit',
+      params: {
+        image: '',
+      },
+    };
+
+    const tree = wrapRender(<EditScreen route={editRouter} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should properly display the feed screen', () => {
+    const tree = wrapRender(<FeedScreen />);
     expect(tree).toMatchSnapshot();
   });
 
@@ -36,6 +66,11 @@ describe('snapshot test', () => {
     };
 
     const tree = wrapRender(<PostScreen route={postRouter} />);
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should properly display the profile screen', () => {
+    const tree = wrapRender(<ProfileScreen />);
     expect(tree).toMatchSnapshot();
   });
 });
